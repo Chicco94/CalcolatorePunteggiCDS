2b2f06e91603f3d6b2d3fc3af3f9237368c67dbe
Enrico Cominato <enrico.cominato@gmail.com>
2021-05-04 11:05:23 +0200

    classe specialità

---
+++ src/classes/specialita.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+class Specialita:
+	def __init__(self,_id,_descr,_categoria):
+		'''id: identificativo fidal
+			descr: nome specialità
+			categoria: codice categoria fidal (ES: 'JF' per junior donne)
+		'''
+		self.id = _id
+		self.descr = _descr
+		self.categoria = _categoria
