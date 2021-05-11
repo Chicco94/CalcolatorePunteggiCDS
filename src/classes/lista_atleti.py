04e2331ca7e3a2c51e2853cc6cfd11f9e5ee5d05
Enrico Cominato <enrico.cominato@gmail.com>
2021-05-11 13:05:06 +0200

    aggiunte funzioni di contorno a lista atleti

--- src/classes/lista_atleti.py
+++ src/classes/lista_atleti.py
@@ -8,7 +8,7 @@ class Lista_Atleti:
 	def __repr__(self):
 		ret_string = ""
 		for atleta in self.lista_atleti:
-			ret_string += "descrizione: {}\tdata nascita: {}\n".format(atleta.descr,atleta.data_nascita.strftime("%d/%m/%Y"))
+			ret_string += "descrizione: {}\tdata nascita: {}\ttot gare: {}\tpieno: {}\n".format(atleta.descr,atleta.data_nascita.strftime("%d/%m/%Y"),atleta.totale_gare+atleta.totale_staffette,atleta.pieno())
 		return ret_string
 
 	def aggiungi_atleta(self,atleta:Atleta) -> Atleta:
@@ -17,3 +17,24 @@ class Lista_Atleti:
 	
 	def get_atleti(self) -> Sequence[Atleta]:
 		return self.lista_atleti
+
+	def get_atleta_by_id(self,idatleta:str) -> Atleta:
+		for a in self.lista_atleti:
+			if (a.id == idatleta):
+				return a
+		return None
+
+	def add_gara_to_atleta(self,atleta:Atleta) -> Atleta:
+		for a in self.lista_atleti:
+			if (a.id == atleta.id):
+				a.totale_gare += 1
+				return a
+		return None
+
+	def add_staffetta_to_atleta(self,atleta:Atleta) -> Atleta:
+		for a in self.lista_atleti:
+			if (a.id == atleta.id):
+				a.totale_staffette += 1
+				return a
+		return None
+
