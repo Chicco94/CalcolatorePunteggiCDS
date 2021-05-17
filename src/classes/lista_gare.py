2f8f4c4e5a5b6ce5f5edeebb26ebb27adfc44d9b
Enrico Cominato <enrico.cominato@gmail.com>
2021-05-17 10:43:27 +0200

    funzione stampa punteggio migliore

--- src/classes/lista_gare.py
+++ src/classes/lista_gare.py
@@ -51,6 +51,7 @@ class Lista_Gare:
 					res_dict[atleta].append(best_risultato)
 		return res_dict
 
+
 	def get_mappa_specialita_risultati_di_atleti(self,lista_atleti:Lista_Atleti)->Dict:
 		'''Ritorna una mappa specialità:[lista dei migliori risultati per quella specialità di ogni atleta]'''
 		temp_dict = self.get_mappa_atleti_risultati_migliori(lista_atleti)
@@ -62,4 +63,59 @@ class Lista_Gare:
 					res_dict[specialita.descr].append(risultato)
 				else:
 					res_dict[specialita.descr] = [risultato]
+		return res_dict
+
+	def copri_gara(self,specialita:Specialita,atleta:Atleta) -> bool:
+		if (specialita.tipologia == "staffetta"):
+			atleta.totale_staffette += 1
+		else:
+			atleta.totale_gare += 1
+		return atleta.pieno()
+	
+	def get_best_mappa_per_staffette(self,lista_atleti:Lista_Atleti)->Dict:
+		temp_dict = self.get_mappa_specialita_risultati_di_atleti(lista_atleti)
+		res_dict = {}
+		# per ogni specialità, se ha solo un punteggio, prendo quello
+		for specialita,risultati in temp_dict.items():
+			if (risultati[0].specialita.tipologia == "staffetta"):
+				if (len(risultati) == 4):
+					res_dict[specialita] = []
+					for risultato in risultati:
+						res_dict[specialita].append(risultato)
+						self.copri_gara(risultato.specialita,lista_atleti.get_atleta_by_id(risultato.atleta.id))
+		return res_dict
+
+	def get_best_mappa_specialita_risultati_di_atleti(self,lista_atleti:Lista_Atleti)->Dict:
+		'''Ritorna la migliore mappa specialità:[lista dei migliori risultati per quella specialità di ogni atleta]
+		che garantisce il più alto punteggio possibile'''
+		temp_dict = self.get_mappa_specialita_risultati_di_atleti(lista_atleti)
+		res_dict = self.get_best_mappa_per_staffette(lista_atleti)
+		# rimuovo le specialità concluse
+		for key in res_dict:
+			temp_dict.pop(key,None)
+
+		# per ogni specialità, se ha solo un punteggio, prendo quello
+		for specialita,risultati in temp_dict.items():
+			if (len(risultati) == 1):
+				risultato:Risultato = risultati[0]
+				res_dict[specialita] = risultato
+				self.copri_gara(risultato.specialita,lista_atleti.get_atleta_by_id(risultato.atleta.id))
+		
+		# rimuovo le specialità concluse
+		for key in res_dict:
+			temp_dict.pop(key,None)
+
+		# per ogni altra specialità, prendo il miglior punteggio per quella specialità se l'atleta non è pieno
+		for specialita,risultati in temp_dict.items():
+			# trovo il miglio punteggio tra quelli proposti
+			best_risultato:Risultato = None
+			best_punteggio:int = 0
+			for risultato in risultati:
+				if (risultato.punteggio > best_punteggio and not lista_atleti.get_atleta_by_id(risultato.atleta.id).pieno()):
+					best_risultato = risultato
+					best_punteggio = risultato.punteggio
+			if (not lista_atleti.get_atleta_by_id(best_risultato.atleta.id).pieno()):
+				res_dict[specialita] = best_risultato
+				self.copri_gara(best_risultato.specialita,lista_atleti.get_atleta_by_id(best_risultato.atleta.id))
+
 		return res_dict
--- src/main.py
+++ src/main.py
@@ -2,15 +2,38 @@ from tinydb import TinyDB, Query
 from classes.lista_gare import Lista_Gare, Gara,Risultato,Specialita
 from classes.lista_atleti import Lista_Atleti, Atleta
 
+def stampa_punteggio_migliore(lista_gare:Lista_Gare,lista_atleti:Lista_Atleti):
+	punteggio_migliore = lista_gare.get_best_mappa_specialita_risultati_di_atleti(lista_atleti)
+	totale_punteggio = 0
+	totale_gare = 0
+	for specialita,risultato in punteggio_migliore.items():
+		print("\n   ---   {}   ---".format(specialita))
+		totale_gare += 1
+		if (type(risultato) == list):
+			for i,ris in enumerate(risultato):
+				if (i == 3):
+					print("{}".format(ris))
+					totale_punteggio += ris.punteggio
+				else:
+					print("{0: <30}".format(ris.atleta.descr))
+		else:
+			print("{}".format(risultato))
+			totale_punteggio += risultato.punteggio
+	print("TOTALE GARE: {}/18\nTOTALE PUNTI: {}\nOBIETTIVO: 13406".format(totale_gare, totale_punteggio))
+
+
 db = TinyDB('./db.json') 
+
+# prendo gli atleti dal database
 atleti = db.table('atleti')
 lista_atleti = Lista_Atleti()
 for atleta in atleti.all():
 	lista_atleti.aggiungi_atleta(atleta)
 
+# prendo le gare dal database
 gare = db.table('gare')
 lista_gare = Lista_Gare()
 for gara in gare.all():
 	lista_gare.aggiungi_gara(gara)
 
-print(lista_gare)
+stampa_punteggio_migliore(lista_gare,lista_atleti)
